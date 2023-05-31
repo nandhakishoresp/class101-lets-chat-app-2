69f948ae8dd2da81d454a7cdb680a1d5dc51f032
nandhakishoresp <122989652+nandhakishoresp@users.noreply.github.com>
2023-05-31 16:25:40 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYJtuAVsqKb-lZvFwHIJS9h4gerNpIZts",
+    authDomain: "greetingsbot-lvnq.firebaseapp.com",
+    databaseURL: "https://greetingsbot-lvnq-default-rtdb.firebaseio.com",
+    projectId: "greetingsbot-lvnq",
+    storageBucket: "greetingsbot-lvnq.appspot.com",
+    messagingSenderId: "1076152412684",
+    appId: "1:1076152412684:web:57ce95cd83b2fff6cd1c29"
+};
